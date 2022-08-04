033773a7b18f340ac450a77a30cc7f6dc733a5da
koleyy <nikolejaixen@gmail.com>
2022-08-04 18:50:15 -0500

    Created test methods for all functions in classes

--- test_classes.py
+++ test_classes.py
@@ -10,26 +10,81 @@ class Test:
         del self.tv_1
 
     def test_init(self):
-        assert self.tv_1.
+        assert self.tv_1.get_channel() == 0
+        assert self.tv_1.get_volume() == 0
+        assert self.tv_1.get_status() == False
 
     def test_power(self):
-        pass
+        self.tv_1.power()
+        assert self.tv_1.get_status() == True
+        self.tv_1.power()
+        assert self.tv_1.get_status() == False
 
     def test_channel_up(self):
-        pass
+        self.tv_1.channel_up()
+        assert self.tv_1.get_channel() == 0
+
+        self.tv_1.power()
+
+        self.tv_1.channel_up()
+        assert self.tv_1.get_channel() == 1
+        self.tv_1.channel_up()
+        assert self.tv_1.get_channel() == 2
+        self.tv_1.channel_up()
+        assert self.tv_1.get_channel() == 3
+        self.tv_1.channel_up()
+        assert self.tv_1.get_channel() == 0
 
     def test_channel_down(self):
-        pass
+        self.tv_1.channel_down()
+        assert self.tv_1.get_channel() == 0
+
+        self.tv_1.power()
+
+        self.tv_1.channel_down()
+        assert self.tv_1.get_channel() == 3
+        self.tv_1.channel_down()
+        assert self.tv_1.get_channel() == 2
+        self.tv_1.channel_down()
+        assert self.tv_1.get_channel() == 1
+        self.tv_1.channel_down()
+        assert self.tv_1.get_channel() == 0
 
     def test_volume_up(self):
-        pass
+        self.tv_1.volume_up()
+        assert self.tv_1.get_volume() == 0
+
+        self.tv_1.power()
+        self.tv_1.volume_up()
+        assert self.tv_1.get_volume() == 1
+        self.tv_1.volume_up()
+        assert self.tv_1.get_volume() == 2
+        self.tv_1.volume_up()
+        assert self.tv_1.get_volume() == 2
 
     def test_volume_down(self):
-        pass
+        self.tv_1.volume_down()
+        assert self.tv_1.get_volume() == 0
+
+        self.tv_1.power()
+        self.tv_1.volume_up()
+        self.tv_1.volume_up()
+        assert self.tv_1.get_volume() == 2
+
+        self.tv_1.volume_down()
+        assert self.tv_1.get_volume() == 1
+        self.tv_1.volume_down()
+        assert self.tv_1.get_volume() == 0
+        self.tv_1.volume_down()
+        assert self.tv_1.get_volume() == 0
+
 
     def test_str(self):
-        pass
-
+        assert self.tv_1.__str__() == 'TV status: Is on = False, Channel = 0, Volume = 0'
+        self.tv_1.power()
+        self.tv_1.volume_up()
+        self.tv_1.channel_up()
+        assert self.tv_1.__str__() == 'TV status: Is on = True, Channel = 1, Volume = 1'
 
 if __name__ == '__main__':
     unittest.main()
