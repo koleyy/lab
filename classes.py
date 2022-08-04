8f246a32c5479090d5dd91372ec030b46323b4cc
koleyy <nikolejaixen@gmail.com>
2022-08-04 17:41:26 -0500

    Added docstrings

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
 class Television:
+    """
+    Class representing details for a remote object
+    """
     MIN_CHANNEL = 0     # Minimum TV channel
     MAX_CHANNEL = 3     # Maximum TV channel
 
@@ -6,17 +9,30 @@ class Television:
     MAX_VOLUME = 2      # Maximum TV volume
 
     def __init__(self):
+        """
+        Constructor that creates the initial state of the television object
+        sets channel and volume to their minimums and status to off
+        :return: None
+        """
         self.__channel = Television.MIN_CHANNEL
         self.__volume = Television.MIN_VOLUME
         self.__status = False
 
     def power(self):
+        """
+        Method that sets the status of the TV to it's opposite when called
+        :return: The status of the TV
+        """
         if not self.__status:
             self.__status = True
         else:
             self.__status = False
 
     def channel_up(self):
+        """
+        Method to raise the channel of the TV object by one
+        :return: The original channel's value +1
+        """
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
@@ -24,6 +40,10 @@ class Television:
                 self.__channel += 1
 
     def channel_down(self):
+        """
+        Method to lower the channel of the TV object by one
+        :return: The original channel's value -1
+        """
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
@@ -31,6 +51,10 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self):
+        """
+        Method to raise the volume of the TV object by one
+        :return: The original volume's value +1
+        """
         if self.__status:
             if self.__volume == Television.MAX_VOLUME:
                 pass
@@ -38,6 +62,10 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self):
+        """
+        Method to lower the volume of the TV object by one
+        :return: The original volume's value -1
+        """
         if self.__status:
             if self.__volume == Television.MIN_VOLUME:
                 pass
@@ -45,4 +73,8 @@ class Television:
                 self.__volume -= 1
 
     def __str__(self):
+        """
+        Method to return the current state of the TV object
+        :return: The status, channel, and volume of the TV object
+        """
         return f'TV status: Is on = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
